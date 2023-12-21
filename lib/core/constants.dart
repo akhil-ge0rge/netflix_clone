ff0837ba38b5058ba8758aac8f4ac9f197d8df0b
Akhil George <akhil8311@gmail.com>
2023-12-21 15:02:59 +0530

    home ui completed

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -9,3 +9,6 @@ const kHeight = SizedBox(
 const kHeight20 = SizedBox(
   height: 20,
 );
+
+const String netflixLogo =
+    'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png';
--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:netflix_clone/core/constants.dart';
-import 'package:netflix_clone/presentation/home/widget/movie_tile_card.dart';
 import 'package:netflix_clone/presentation/home/widget/top_ten_movie_card.dart';
 
 import '../../core/colors/colors.dart';
-import '../widget/main_title.dart';
 import 'widget/movie_card_list_with_text.dart';
+
+ValueNotifier<bool> scrollNotifier = ValueNotifier(true);
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -14,99 +15,214 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListView(
-          children: [
-            Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 500,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          fit: BoxFit.cover, image: NetworkImage(imageUrl))),
+        body: ValueListenableBuilder(
+            valueListenable: scrollNotifier,
+            builder: (ctx, val, _) {
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: NotificationListener<UserScrollNotification>(
+                  onNotification: (notification) {
+                    final ScrollDirection scrollDirection =
+                        notification.direction;
+                    if (scrollDirection == ScrollDirection.reverse) {
+                      scrollNotifier.value = false;
+                    } else if (scrollDirection == ScrollDirection.forward) {
+                      scrollNotifier.value = true;
+                    }
+                    return true;
+                  },
+                  child: Stack(
+                    children: [
+                      ListView(
+                        physics: const ClampingScrollPhysics(),
+                        children: [
+                          Stack(
+                            alignment: Alignment.bottomCenter,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                height: 500,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage(imageUrl))),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Icon(
+                                        Icons.add,
+                                        color: kWhiteColor,
+                                        size: 30,
+                                      ),
+                                      Text(
+                                        "My List",
+                                        style: TextStyle(
+                                            color: kWhiteColor,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18),
+                                      ),
+                                    ],
+                                  ),
+                                  TextButton.icon(
+                                    onPressed: () {},
+                                    style: const ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStatePropertyAll(kWhiteColor),
+                                    ),
+                                    icon: const Icon(
+                                      Icons.play_arrow,
+                                      color: kBlackColor,
+                                    ),
+                                    label: const Text(
+                                      "Play",
+                                      style: TextStyle(
+                                          color: kBlackColor,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20),
+                                    ),
+                                  ),
+                                  Column(
+                                    children: [
+                                      Icon(
+                                        Icons.info_outline,
+                                        color: kWhiteColor,
+                                        size: 30,
+                                      ),
+                                      Text(
+                                        "Info",
+                                        style: TextStyle(
+                                            color: kWhiteColor,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                          kHeight20,
+                          MovieCardListWithTextWidget(
+                            title: "Released in the past year",
+                          ),
+                          kHeight20,
+                          MovieCardListWithTextWidget(
+                            title: "Trending Now",
+                          ),
+                          kHeight20,
+                          TopTenMovieCardWidget(
+                            title: "Top 10 TV Shows in India Today",
+                          ),
+                          kHeight20,
+                          MovieCardListWithTextWidget(
+                            title: "Tense Dramas",
+                          ),
+                          kHeight20,
+                          MovieCardListWithTextWidget(
+                            title: "South Indian Cinemas",
+                          ),
+                        ],
+                      ),
+                      scrollNotifier.value
+                          ? AnimatedContainer(
+                              duration: Duration(milliseconds: 1000),
+                              child: Container(
+                                height: 80,
+                                width: double.infinity,
+                                color: kBlackColor.withOpacity(0.1),
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Image.network(
+                                                netflixLogo,
+                                                width: 40,
+                                                height: 40,
+                                              ),
+                                            ],
+                                          ),
+                                          Row(
+                                            children: [
+                                              const Icon(
+                                                Icons.cast,
+                                                size: 30,
+                                                color: kWhiteColor,
+                                              ),
+                                              kWidth,
+                                              Container(
+                                                width: 40,
+                                                height: 40,
+                                                color: Colors.blue,
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                      kHeight,
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Text(
+                                            "TV Shows",
+                                            style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          Text(
+                                            "Movies",
+                                            style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          Row(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                "Category",
+                                                style: TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              Icon(
+                                                Icons.arrow_drop_down_sharp,
+                                                color: kWhiteColor,
+                                              )
+                                            ],
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
+                          : SizedBox()
+                    ],
+                  ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.add,
-                          color: kWhiteColor,
-                          size: 30,
-                        ),
-                        Text(
-                          "My List",
-                          style: TextStyle(
-                              color: kWhiteColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
-                      ],
-                    ),
-                    TextButton.icon(
-                      onPressed: () {},
-                      style: const ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll(kWhiteColor),
-                      ),
-                      icon: const Icon(
-                        Icons.play_arrow,
-                        color: kBlackColor,
-                      ),
-                      label: const Text(
-                        "Play",
-                        style: TextStyle(
-                            color: kBlackColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20),
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.info_outline,
-                          color: kWhiteColor,
-                          size: 30,
-                        ),
-                        Text(
-                          "Info",
-                          style: TextStyle(
-                              color: kWhiteColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              ],
-            ),
-            kHeight20,
-            MovieCardListWithTextWidget(
-              title: "Released in the past year",
-            ),
-            kHeight20,
-            MovieCardListWithTextWidget(
-              title: "Trending Now",
-            ),
-            kHeight20,
-            TopTenMovieCardWidget(
-              title: "Top 10 TV Shows in India Today",
-            ),
-            kHeight20,
-            MovieCardListWithTextWidget(
-              title: "Tense Dramas",
-            ),
-            kHeight20,
-            MovieCardListWithTextWidget(
-              title: "South Indian Cinemas",
-            ),
-          ],
-        ),
-      ),
-    );
+              );
+            }));
   }
 }
